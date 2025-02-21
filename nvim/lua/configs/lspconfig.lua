91b2ebbb70a695b60df993ddbb66c5e53acf7581
Grant Foster <gfoster92@gmail.com>
2025-02-21 16:44:26 -0500

    golang support

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,11 +1,21 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
+local util = require "lspconfig.util"
 
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
 local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
+
+require("lspconfig").gopls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
