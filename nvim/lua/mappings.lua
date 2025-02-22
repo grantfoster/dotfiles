ef890026f38905539316b304bd34ae34f1bac042
Grant Foster <gfoster92@gmail.com>
2025-02-21 20:25:37 -0500

    added terminal esc mapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,4 +7,4 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("t", "<ESC><ESC>", "<C-\\><C-n>", { desc = "Exit terminal" })
